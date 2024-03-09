a869a96099f6fb1739264872bf4ea4d5826ec427
Bhatck-Aegol <aleixoguilherme2009@gmail.com>
2024-03-09 10:21:37 -0300

    Added README, Optimised build_vertices Changed the build_vertices connecting version part from 2 for loops to only 1.

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+This is a project of a geometric calculator, so you can input shapes and values and it will calculate unknown values.
+It will have a GUI where you can insert these shapes, or use some kind of API to return the requested value.
+
+# Contributing
+- Any help with the project will be appreciated, as i'm making this without knowledge of some techniques.
+- I request that you don't copy code from other projects and that you try to build the code yourself, as i'm not worried with any optimisations yet.
+  - But if optimisation does become a problem for certain calculations, which i'm sure it will, mark it as non automatic in the code *(Which is not implemented yet)*.
--- classes.py
+++ classes.py
@@ -5,12 +5,15 @@ class Fractional_number:
     def __init__(self, numerator: int, denominator: int):
         self.numerator = numerator
         self.denominator = denominator
+
     def __add__(self, number: int):
         return Fractional_number(self.numerator + number * self.denominator, self.denominator)
+    
     def __add__(self, number: Fractional_number):
         if self.denominator == number.denominator:
             return Fractional_number(self.numerator + number.numerator, self.denominator)
         return Fractional_number(self.numerator * number.denominator + number.numerator * self.denominator, self.denominator * number.denominator)
+    
     def __str__(self):
         return f"{self.numerator}/{self.denominator}"
         
@@ -20,6 +23,7 @@ class Vertice:
         if connected_to is None: # REALLY IMPORTANT PART
             connected_to = [] # DO NOT DELETE
         self.connected_to: list = connected_to
+
     def connect_to(self, other_vertice, one_way = False):
         self.connected_to.append(other_vertice)
         if not one_way:
@@ -27,17 +31,19 @@ class Vertice:
 
 
 class Shape:
-    def __init__(self, sides: int, sides_size) -> None:
+    def __init__(self, sides: int) -> None:
         self.sides: int = sides
         self.vertices: list = self.build_vertices(sides)
+
     def build_vertices(self, sides: int):
         vertices = []
         for _ in range(sides):
             vertices.append(Vertice())
-        
-        for vertice in vertices:
-            for other_vertice in vertices:
-                if vertice != other_vertice:
-                    vertice.connect_to(other_vertice, one_way = True)
+
+        # Goes backwards through the vertices and connects them.
+        # This was simplified from the previous code, now using only one for loop and having a complexity of O(N)
+        # Compared to the previous code that had a complexity of O(N^2)
+        for i in range(len(vertices)):
+            vertices[-i].connect_to(vertices[-i - 1])
 
         return vertices
